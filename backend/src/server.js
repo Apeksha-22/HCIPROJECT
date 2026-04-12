828e756d08ac3f727b269565aa583120ef5c4cbf
Apeksha-22 <morenaapekshajain2209@gmail.com>
2026-04-12 23:01:17 +0530

    fix api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,9 +18,9 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://mess-menufeedback.netlify.app",
   credentials: true
-}))
+}));
 app.use(express.json())
 
 // MongoDB Connection
